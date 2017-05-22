c4ec5649444bd29cfa66afe877265c6dfa9c920c
trentspi <tspice23@att.net>
2017-05-22 16:47:59 -0400

    made src folders for seperation, footer is now rendered as a component

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header'
-import './App.css';
+import Footer from './Footer'
+import '../css/App.css';
 
 class App extends Component {
   render() {
@@ -50,7 +51,7 @@ class App extends Component {
               <div className="ad-image">
                 <img src="https://s-media-cache-ak0.pinimg.com/originals/b4/d5/ab/b4d5ab4a82b8f7efc1adaf548df1d089.jpg" alt="hammer" />
                 <a href="#">visit sponsor</a>
-              </div>  
+              </div>
               <h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
               <p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
 
@@ -64,33 +65,30 @@ class App extends Component {
               <a href="#">
                 <img src="https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif" alt="orc" />
                 <p>Single Orcs in Indianapolis</p>
-              </a>  
+              </a>
             </div>
             <div className="small-6 medium-3 columns other-article">
               <a href="#">
                 <img src="https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg" alt="mountain" />
                 <p>You won't believe what's under this mountain</p>
-              </a>  
+              </a>
             </div>
             <div className="small-6 medium-3 columns other-article">
               <a href="#">
                 <img src="http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg" alt="gold" />
                 <p>Mine 20% more gold with One Weird Trick</p>
-              </a> 
+              </a>
             </div>
             <div className="small-6 medium-3 columns other-article">
               <a href="#">
                 <img src="http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg" alt="hobbit" />
                 <p>Surprise for Indiana Hobbits born before 1999</p>
-              </a> 
+              </a>
             </div>
           </div>
         </main>
 
-        <footer className="expanded row">
-          <h6>&copy; 2017 Erebor Industries</h6>
-          <h6 className="float-right">All rights reserved</h6>
-        </footer>
+        <Footer />
 
       </div>
     );
