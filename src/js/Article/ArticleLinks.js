4f2f5c176f958f4fb08a60626518a2aa91842af9
trentspi <tspice23@att.net>
2017-05-23 14:36:48 -0400

    Added comments section under article buttons

--- src/js/Article/ArticleLinks.js
+++ src/js/Article/ArticleLinks.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import Comments from './Comments';
 import '../../css/ArticleLinks.css';
 
 class ArticleLinks extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showComments: false
+    }
+
+    this.toggleComments = this.toggleComments.bind(this);
+  }
+
+  toggleComments(ev) {
+    ev.preventDefault();
+    this.setState({
+      showComments: !this.state.showComments
+    });
+    //() => console.log(this.state));
+  }
+
   render() {
     return (
       <div className="article-links">
-        <a className="article-link" href="#">
+        <a className="article-link" href="#" onClick={this.toggleComments}>
           <i className="fa fa-comments-o"></i>
           <span className="article-link-text">Comments</span>
         </a>
@@ -13,7 +32,10 @@ class ArticleLinks extends Component {
           <i className="fa fa-share"></i>
           <span className="article-link-text">Share Post</span>
         </a>
+        {this.state.showComments ? <Comments />: null }
       </div>
+
+
     )
   }
 }
