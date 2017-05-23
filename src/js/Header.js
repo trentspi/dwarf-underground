00d433d431fbde319e7825e45fe481912d3a8084
trentspi <tspice23@att.net>
2017-05-23 14:14:01 -0400

    Moved Nav Links outside of header component, mapped as prop

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import '../css/Header.css';
+import NavLinks from './NavLinks';
+
+const links = ['Home', 'About', 'Log in'];
 
 class Header extends Component {
+
   render() {
     return (
       <div className="expanded row header">
@@ -10,9 +14,7 @@ class Header extends Component {
             <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
           </div>
           <div className="float-right nav-links">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Log in</a>
+            <NavLinks links={links}/>
           </div>
         </div>
         <div className="headline text-center small-12 columns">
