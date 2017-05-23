e2ec8320152e93fee66768e3fab1a843b7926710
trentspi <tspice23@att.net>
2017-05-23 10:02:49 -0400

    Header now renders as a component

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import '../css/Header.css'
+import React, { Component } from 'react';
+import '../css/Header.css';
 
 class Header extends Component {
   render() {
@@ -24,4 +24,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default Header;
